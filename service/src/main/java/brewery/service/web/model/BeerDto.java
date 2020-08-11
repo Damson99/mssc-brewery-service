e634a38f2d2234f4cf087af069770a9d5c1d8ac1
DaMSoN <damian.wieczorek99@gmail.com>
2020-08-11 22:28:02 +0200

    adding validation. Closes #8

--- service/src/main/java/brewery/service/web/model/BeerDto.java
+++ service/src/main/java/brewery/service/web/model/BeerDto.java
@@ -5,6 +5,12 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 
@@ -14,8 +20,31 @@ import java.util.UUID;
 @Builder
 public class BeerDto
 {
+    @Null
     private UUID id;
+
+    @Null
+    private Integer version;
+
+    @Null
+    private OffsetDateTime createdDate;
+
+    @Null
+    private OffsetDateTime modifiedDate;
+
+    @NotBlank
     private String beerName;
+
+    @NotNull
     private String beerStyle;
+
+    @NotNull
+    @Positive
     private Long upc;
+
+    @NotNull
+    @Positive
+    private BigDecimal price;
+
+    private Integer quantityOnHand;
 }
