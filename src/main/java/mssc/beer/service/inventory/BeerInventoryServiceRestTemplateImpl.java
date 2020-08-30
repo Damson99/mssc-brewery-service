5214d967bfebf9da439b5fb20b85771c3c17ff7e
DaMSoN <damian.wieczorek99@gmail.com>
2020-08-30 21:28:37 +0200

    Refactoring path for common packages between mssc. Closes #22

--- src/main/java/mssc/beer/service/inventory/BeerInventoryServiceRestTemplateImpl.java
+++ src/main/java/mssc/beer/service/inventory/BeerInventoryServiceRestTemplateImpl.java
@@ -15,7 +15,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Slf4j
-@ConfigurationProperties(prefix = "sfg.brewery", ignoreUnknownFields = false)
+@ConfigurationProperties(prefix = "mssc.brewery", ignoreUnknownFields = false)
 @Component
 public class BeerInventoryServiceRestTemplateImpl implements BeerInventoryService
 {
